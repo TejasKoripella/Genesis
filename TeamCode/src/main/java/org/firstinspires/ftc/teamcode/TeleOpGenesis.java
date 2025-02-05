8b17b514276be81a36a9b9585513f38763cf718c
TejasKoripella <koripellatejas@gmail.com>
2025-02-04 20:38:37 -0600

    Here is teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpGenesis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpGenesis.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "TeleOp")
+@TeleOp(name = "TeleOpGenesis")
 public class TeleOpGenesis extends LinearOpMode {
 
     public DcMotor  FrontRight;
